b66968dda0ec0570686f4be60a13d3d06dd8e48f
Samuel Marguerat <smarguer@mrc-8384.cscdom.csc.mrc.ac.uk>
2013-04-24 09:41:09 +0100

    change 190413

--- analysis_local_100413.r
+++ analysis_local_100413.r
@@ -1,7 +1,7 @@
- 
- cs_gff=read.delim("EXOSOME_local/0304_1_RITA.pA.map",stringsAsFactors=F)
+ cs_gff=read.delim("pA_analysis/0304_1_RITA.pA.map",stringsAsFactors=F)
+ #cs_gff=read.delim("EXOSOME_local/0304_1_RITA.pA.map",stringsAsFactors=F)
  type="cs"
- cs="main_cs"
+ cs="distal_cs"
  an=cs_gff
 
  test=list()
@@ -11,6 +11,7 @@
  }
  names(test)=colnames(chr1)
  #stop("so far so good...")
+ pdf("figure_TTS_180413.pdf",width=30,height=10)
  par(mfrow=c(2,4))
  ct=c(1,3,5,5,5,5,10)
  ex=c(2,4,6,7,8,9,11)
@@ -23,8 +24,9 @@
   }
   else
   {
-   plotTTS(test[c(ct[i],ex[i])],my.ylim=c(500,2000))
+   plotTTS(test[c(ct[i],ex[i])],my.ylim=c(500,1500))
   }
+  legend(x="topleft",legend=names(test)[ex[i]],bty="n",cex=3)
  }
  
  #plotTTS(test[c(3,4,5,6)],my.ylim=c(500,1500),my.col=c("black","red","black","red"))
@@ -32,4 +34,5 @@
  plot(out,pch=20,col="red",cex=3,xlim=c(0,300),ylim=c(1,2))
  points(out[c(1,3,5,10),],pch=20,col="black",cex=3)
  textxy(out[,1],out[,2],labs=rownames(out),cx=1.2)
-
+ dev.off()
+ rm(an,cs,ct,ex,i,type)
